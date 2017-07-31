efd37973aec7babced2b2b2718b3e912091751f1
Anton <atour07@yandex.ru>
2017-07-31 22:07:37 +0400

    Card verification needded about 15

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
+
+
 
 
 var routes = require('./routes/index');
@@ -35,7 +38,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret:'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(session({
+    secret:'mysupersecret',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection}),
+    cookie: {maxAge: 180 * 60 * 100}
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -43,6 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
       res.locals.login = req.isAuthenticated();
+      res.locals.session = req.session;
       next();
     });
 
