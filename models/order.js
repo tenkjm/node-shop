8711025ff321394fa6b9542bde61915262a53bba
Anton <atour07@yandex.ru>
2017-08-01 22:07:29 +0400

    Sime lessons

---
+++ models/order.js
@@ -0,0 +1,15 @@
+/**
+ * Created by user on 01.08.2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    user:{type: Schema.Types.ObjectId, ref:'User'},
+    cart: {type: Object, required: true},
+    address: {type: String, required: true},
+    name: {type: String, required: true},
+    paymentId: {type: String, required: true}
+});
+
+module.exports = mongoose.model('Order', schema)
